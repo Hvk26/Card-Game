b21b13826383fcf959b18678934a9a9efcefdf5b
Hvk26 <hvk8789@gmail.com>
2024-04-18 01:14:52 +0530

    Adding local storage into game to create save point for game reloading

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
-// To get the deck Id using respective API
-let deckId = ''
-fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    deckId = data.deck_id
-  })
-  .catch(err => {
-    console.log(`error ${err}`)
-  })
+
+document.querySelector('.fetch').addEventListener('click', fetchNewDeck)
+document.querySelector('#player1').src = localStorage.getItem('cardofPlayer1')
+document.querySelector('#player2').src = localStorage.getItem('cardofPlayer2')
+document.querySelector('h3').innerText = localStorage.getItem('currentWinner')
+// To get the deck Id using respective API bu using function
+function fetchNewDeck() {
+  localStorage.clear()
+  fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      console.log(data.deck_id)
+
+      //To store the desk Id in local Storage
+      localStorage.setItem('currentDeckId', data.deck_id)
+
+
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    })
+  document.querySelector('#player1').src = ''
+  document.querySelector('#player2').src = ''
+}
 
 // To get two shiffled card displayed at deck
-document.querySelector('button').addEventListener('click', drawTwo)
+document.querySelector('.deal').addEventListener('click', drawTwo)
 
 let p1count = 0;
 let p2count = 0;
 
 function drawTwo() {
-  const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+  const url = `https://www.deckofcardsapi.com/api/deck/${localStorage.getItem('currentDeckId')}/draw/?count=2`
 
   fetch(url)
     .then(res => res.json()) // parse response as JSON
@@ -30,21 +44,23 @@ function drawTwo() {
       let remainingVal = data.remaining;
       console.log(remainingVal)
       winDecider(remainingVal)
-
+      localStorage.setItem('cardofPlayer1', data.cards[0].image)
+      localStorage.setItem('cardofPlayer2', data.cards[1].image)
       if (player1Val > player2Val) {
         document.querySelector('h3').innerText = 'Player 1 wins'
         p1count += 1;
-        console.log('p1 '+ p1count)
+        console.log('p1 ' + p1count)
       }
       else if (player1Val < player2Val) {
         document.querySelector('h3').innerText = 'Player 2 wins'
         p2count += 1;
-        console.log('p2 '+p2count)
+        console.log('p2 ' + p2count)
       }
       else {
         document.querySelector('h3').innerText = 'Draw'
       }
-      
+      localStorage.setItem('currrentWinner', document.querySelector('h3').innerText)
+
     })
     .catch(err => {
       console.log(`error ${err}`)
@@ -59,26 +75,13 @@ function convertToNum(val) {
   else { return Number(val); }
 }
 
-function winDecider(value){
-if(value === 0){
-  if(p1count > p2count){
-    document.querySelector('.final').innerText = 'congratulationsss Player 1 you won'
+function winDecider(value) {
+  if (value === 0) {
+    if (p1count > p2count) {
+      document.querySelector('.final').innerText = 'congratulationsss Player 1 you won'
+    }
+    else if (p1count < p2count) {
+      document.querySelector('.final').innerText = 'congratulationsss Player 2 you won'
+    }
   }
-  else if(p1count < p2count){
-    document.querySelector('.final').innerText = 'congratulationsss Player 2 you won'
-  }
-}
-}
-
-if (!localStorage.getItem('botScore')) {
-  localStorage.setItem('botScore', 0)
-}
-document.querySelector('button').addEventListener('click', addAnotherOne)
-
-function addAnotherOne() {
-
-  let botScoreVal = Number(localStorage.getItem('botScore'))
-  botScoreVal += 1
-  localStorage.setItem('botScore', botScoreVal)
-
 }
